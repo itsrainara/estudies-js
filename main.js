95a131cf23fcb7f39db6ce28ff4fe14408ce2f0f
rainara Silva <rainara.cps@gmail.com>
2022-09-18 12:18:45 -0300

    new

---
+++ main.js
@@ -0,0 +1,17 @@
+// Named Export => importar sempre com o mesmo nome da exportação
+// import { soma, sub, PI } from './lib/math'
+
+// console.log(soma(1, 4))
+// console.log(sub(9, 3))
+// console.log(PI)
+
+//usar o export default = quando quero exportar uma unica função de alguma pasta/arquivo, e posso mudar o nome dela(porem não é recomendado)
+
+//caso queira exportar tudo de um arquivo
+
+// import * as math from './lib/math'
+
+//caso queira importar uma função e renomear ela
+// import { soma as sum } from './lib/math'
+
+// console.log(sum(1, 3))
